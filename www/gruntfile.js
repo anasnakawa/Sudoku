4ede3b7b494bafecc5997e63d5c2380934353a1a
Anas Nakawa <anas.nakawa@gmail.com>
2014-03-07 18:07:48 +0400

    adding some components

---
+++ www/gruntfile.js
@@ -0,0 +1,23 @@
+/*!
+ * grunt main task file that 
+ * will power our rocking game
+ */
+
+module.exports = function( grunt ) {
+
+  grunt.initConfig({
+
+  });
+
+  // load tasks
+  // ----------
+  // official
+  grunt.loadNpmTasks( 'grunt-contrib-copy' );
+  grunt.loadNpmTasks( 'grunt-contrib-concat' );
+  grunt.loadNpmTasks( 'grunt-contrib-watch' );
+  // other
+
+  // define tasks
+  // ------------
+
+}
