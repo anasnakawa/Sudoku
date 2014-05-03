ec2afba24dd6d212097f096d56fd17124a9af0ab
Javool <jamalitcis@gmail.com>
2014-05-03 19:29:22 +0400

    Create Fixed Solver for the predefined Games Generator #6
    in addition creating online suduko solver ...commented Out

---
+++ www/js/engine/fixedSolver.ts
@@ -0,0 +1,52 @@
+﻿module Sudoku {
+    export class fixedSolver implements baseSolver {
+        constructor(solutionsGetter: () => any[]) {
+            this.getAllSolutions = solutionsGetter;
+        }
+        private getAllSolutions: () => any[];
+        public solve(board: board): boolean {
+            var succeed = false;
+            var game: string = "";
+            var solution: string = null;
+            var userSolution: string = "";
+            var solutions = this.getAllSolutions();
+            //construct Searcher 
+            for (var r = 0; r < 9; r++) {
+                // columns
+                for (var c = 0; c < 9; c++) {
+                    game += (board.getCell(r, c).orignalData.toString());
+                    userSolution += (board.getCell(r, c).data.toString());
+                }
+            }
+            var index = 0;
+            //find game 
+            for (; index < solutions.length; index++) {
+                if (solutions[index]["sud"] == game) {
+                    solution = solutions[index]["sol"];
+                    break;
+                }
+            }
+            //validate Solution if is valid
+            if (solution != null) {
+                for (var charIndex = 0; charIndex < solution.length; charIndex++) {
+                    if (userSolution.charAt(charIndex) !="0" && userSolution.charAt(charIndex) != solution.charAt(charIndex)) {
+                        break;
+                    }
+                }
+                if (charIndex == solution.length) {//then its succeed
+                    succeed = true;
+                    //fill the solution
+                    for (var r = 0; r < 9; r++) {
+                        for (var c = 0; c < 9; c++) {
+                            board.setSystemCell(r, c, parseInt(solution.charAt((r * 9) + c)));
+                        }
+                    }
+                }
+            }
+
+            return succeed;
+        }
+
+
+    }
+} 
---
+++ www/js/engine/generator.js
@@ -0,0 +1,14 @@
+﻿var Sudoku;
+(function (Sudoku) {
+    var generator = (function () {
+        function generator() {
+        }
+        generator.prototype.genrate = function (board, level) {
+            var succeed = false;
+            return succeed;
+        };
+        return generator;
+    })();
+    Sudoku.generator = generator;
+})(Sudoku || (Sudoku = {}));
+//# sourceMappingURL=generator.js.map
